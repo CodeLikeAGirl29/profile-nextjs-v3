4812cb3208bb31c0eab673bac532e2bea5705039
codelikeagirl29 <codelikeagirl91@gmail.com>
2023-06-22 04:48:58 -0500

    added google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
